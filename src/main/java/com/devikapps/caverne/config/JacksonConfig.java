78a7593ce6fbc824c03a4041ef626e32ebeef595
andomamy <hei.ando14@gmail.com>
2026-04-01 23:20:04 +0300

    feat: jacksonconfig for mapping

--- src/main/java/com/devikapps/caverne/config/JacksonConfig.java
+++ src/main/java/com/devikapps/caverne/config/JacksonConfig.java
@@ -1,18 +1,24 @@
 package com.devikapps.caverne.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.json.JsonMapper;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JacksonConfig {
 
     @Bean
-    public ObjectMapper jacksonCustomizer() {
+    public ObjectMapper objectMapper() {
         return JsonMapper.builder()
+                .findAndAddModules()
                 .propertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
+                .enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING)
+                .enable(DeserializationFeature.READ_ENUMS_USING_TO_STRING)
                 .build();
     }
+
 }
