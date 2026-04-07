b606e378e924bfacdbc25f2d881026735d2180d3
andomamy <hei.ando14@gmail.com>
2026-04-07 23:37:13 +0300

    feat: authentication implementation

--- src/test/java/com/devikapps/caverne/modules/payment/PaymentMethodApiIntegrationTest.java
+++ src/test/java/com/devikapps/caverne/modules/payment/PaymentMethodApiIntegrationTest.java
@@ -10,11 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @Import(TestcontainersConfiguration.class)
+@TestPropertySource(properties = "stripe.enabled=false")
 class PaymentMethodApiIntegrationTest {
 
   @Autowired private MockMvc mockMvc;
